c0287dcc2baa75e4c1364f221d14e2c5a2647ba8
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-11-18 08:45:54 +0100

    Add files via upload

---
+++ halmaz/Program.cs
@@ -0,0 +1,12 @@
+﻿using halmaz;
+
+Set halmaz = new Set(10);
+halmaz.Add(1.5);
+halmaz.Add(2.0);
+halmaz.Add(3.5);
+
+Console.WriteLine($"A halmaz jelenlegi hossza: {halmaz.Length()}");
+Console.WriteLine("Elem eltávolítása:");
+halmaz.Remove(2.0);  
+Console.WriteLine($"A halmaz jelenlegi hossza: {halmaz.Length()}");
+Console.WriteLine($"A halmaz kapacitása: {halmaz.GetSize()}");
---
+++ halmaz/halmaz.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace halmaz
+{
+    public class Set
+    {
+        private double[] items;
+        private int count;
+        private int capacity;
+
+
+        public Set() { }
+        public Set(int initialCapacity = 100)
+        {
+            this.capacity = initialCapacity;
+            this.items = new double[capacity];
+            this.count = 0;
+        }
+
+  
+        public void Add(double elem)
+        {
+          
+            if (Contains(elem))
+            {
+                Console.WriteLine("Az elem már létezik a halmazban!");
+                return;
+            }
+
+            if (count == capacity)
+            {
+                Console.WriteLine("A halmaz megtelt!");
+                return;
+            }
+
+            items[count] = elem;
+            count++;
+        }
+
+        public void Remove(double elem)
+        {
+            int index = FindIndex(elem);
+
+            if (index == -1)
+            {
+                Console.WriteLine("Az elem nem található a halmazban!");
+                return;
+            }
+
+            for (int i = index; i < count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            count--;
+        }
+
+        public bool Contains(double elem)
+        {
+            return FindIndex(elem) != -1;
+        }
+
+
+        private int FindIndex(double elem)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == elem)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int Length()
+        {
+            return count;
+        }
+
+        public int GetSize()
+        {
+            return capacity;
+        }
+    }
+}
